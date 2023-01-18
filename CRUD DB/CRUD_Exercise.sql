10cc917141c0c100ea15e3c5f9c48bbd0b2349c7
Martin Milev <marto379@abv.bg>
2023-01-18 16:13:45 +0200

    Not complete

---
+++ CRUD DB/CRUD_Exercise.sql
@@ -0,0 +1,19 @@
+--TASK2
+SELECT * FROM Departments
+--TASK3
+SELECT Name FROM Departments
+--TASK4
+SELECT FirstName, LastName, Salary FROM Employees
+--TASK5
+SELECT FirstName
+, MiddleName
+, LastName 
+FROM Employees
+--TASK6
+SELECT FirstName + '.' + LastName + '@softuni.bg' 
+AS [Full Email Address] 
+FROM Employees
+--TASK7
+SELECT DISTINCT Salary FROM Employees 
+--TASK8
+SELECT * FROM Employees
